c3fcf59ece60df0a3cf8b8fa1b930651f09ce7dc
m0rt <mort@paradox.zone>
2025-08-10 00:08:49 -0400

    rework

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,38 @@
-import { svelte } from '@sveltejs/vite-plugin-svelte';
+import devtoolsJson from 'vite-plugin-devtools-json';
+import tailwindcss from '@tailwindcss/vite';
+import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  plugins: [svelte()],
-  server: { port: 5173, host: true }
+	plugins: [tailwindcss(), sveltekit(), devtoolsJson()],
+    server: { port: 5173, host: true },
+	test: {
+		expect: { requireAssertions: true },
+		projects: [
+			{
+				extends: './vite.config.ts',
+				test: {
+					name: 'client',
+					environment: 'browser',
+					browser: {
+						enabled: true,
+						provider: 'playwright',
+						instances: [{ browser: 'chromium' }]
+					},
+					include: ['src/**/*.svelte.{test,spec}.{js,ts}'],
+					exclude: ['src/lib/server/**'],
+					setupFiles: ['./vitest-setup-client.ts']
+				}
+			},
+			{
+				extends: './vite.config.ts',
+				test: {
+					name: 'server',
+					environment: 'node',
+					include: ['src/**/*.{test,spec}.{js,ts}'],
+					exclude: ['src/**/*.svelte.{test,spec}.{js,ts}']
+				}
+			}
+		]
+	}
 });
